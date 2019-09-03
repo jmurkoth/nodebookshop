c90ca2798719718d9b118a66c467dc85d0f166a8
Murkoth, Jeevan <jeevan.murkoth@intel.com>
2019-09-02 21:52:48 -0700

    product display page first cut

--- controllers/shopcontroller.js
+++ controllers/shopcontroller.js
@@ -1,11 +1,14 @@
 const express= require('express');
-
+const Product  = require('../models/Product');
 module.exports.getCart=(req,res)=>{
     res.render('shop/cart.ejs',{path:'/cart'});
 }
 
 module.exports.getProducts=(req,res)=>{
-    res.render('shop/products.ejs',{path:'/products'});
+    Product.getAll((prods)=>{
+        res.render('shop/products.ejs',{path:'/products',products:prods});
+    })
+    
 }
 
 module.exports.getOrders=(req,res)=>{
